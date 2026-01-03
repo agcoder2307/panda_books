4fa5bada6ddd822103e8e2089c29aba07fc27579
agcoder2307 <abbosxd@mail.ru>
2026-01-03 14:37:45 +0500

    build fixed

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 // @ts-ignore
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store, persistor } from "./app/store.js";
+import { store, persistor } from "./app/store.ts";
 import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
